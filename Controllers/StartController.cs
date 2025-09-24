d481a3515db65bcc0161a81914a9adeb5495e33f
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-09-24 09:17:21 +0200

    added hangfire

---
+++ Controllers/StartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Umbraco_001.Controllers;
+
+public class StartController : RenderController
+{
+    public StartController(ILogger<RenderController> logger) 
+    { 
+        
+    }
+
+    public override IActionResult Index()
+    {
+        return View();
+    }
+}
